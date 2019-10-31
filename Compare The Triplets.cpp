65f7c0cf84c554886500bce8cbcd5633027e5d09
vrajpatel123 <49654952+vrajpatel123@users.noreply.github.com>
2019-10-31 14:18:02 +0530

    Update Compare The Triplets.cpp

--- Compare The Triplets.cpp
+++ Compare The Triplets.cpp
@@ -1,12 +1,9 @@
 #include<iostream>
 using namespace std;
 int main() {
- 
     int a[3],b[3],aPoint = 0,bPoint = 0;
- 
     cin >> a[0] >> a[1] >> a[2];
     cin >> b[0] >> b[1] >> b[2];
- 
     for(int i = 0; i < 3; i++){
         if(a[i] > b[i])aPoint++;
         else if(a[i] < b[i])bPoint++;
@@ -14,4 +11,3 @@ int main() {
     cout << aPoint << " " << bPoint << endl;
     return 0;
 }
-
